63f473d392a1065eb0784955613c7d49035f197a
saikrishnama <mandavasaikrishna91@gmail.com>
2021-03-01 09:53:22 -0500

    Update sns.tf

--- sns.tf
+++ sns.tf
@@ -5,9 +5,9 @@
      display_name = var.sns_display_name
  }
 
- resource "aws_sns_topic_subscription" "default" {
-     count = var.enable_sns_subscription == true ? 1 : 0
-     topic_arn = ""
-     protocol = ""
-     endpoint = ""
- }
+ #resource "aws_sns_topic_subscription" "default" {
+  #   count = var.enable_sns_subscription == true ? 1 : 0
+   #  topic_arn = ""
+    # protocol = ""
+    # endpoint = ""
+ #}
